9df3d0245601efb95e885d94f7988f5041591ccd
johnsu01 <>
2004-12-06 09:49:54 +0000

    moved delicious sources to proper place in tree.
    20041206094954-219f2-0de1acc0121794dcd6e94bc911d0ac854d0bf0f1.gz

---
+++ lisp/delicious/maketar.sh
@@ -0,0 +1,4 @@
+
+cd /home/johnsu01/documents/cvs/code/lisp
+tar cvzf delicious-el.tar.gz ./delicious/delicious.el ./delicious/delicioapi.el ./delicious/README ./delicious/ChangeLog
+scp delicious-el.tar.gz freeshell.org:~/html/downloads/
