6cb5a82519356ff381503cb957e89158bc355ce7
johnsu01 <>
2004-12-06 11:39:38 +0000

    Added license to the archive for real. tar script now generates the changelog and copies over to the server.
    20041206113938-219f2-03cecc53926f180d70f619ed613ebf84a9610c72.gz

--- lisp/delicious/maketar.sh
+++ lisp/delicious/maketar.sh
@@ -1,3 +1,5 @@
 
+cvs log > CHANGELOG
 cd /home/johnsu01/documents/cvs/code/lisp
-tar cvzf delicious-el.tar.gz ./delicious/delicious.el ./delicious/delicioapi.el
+tar cvzf delicious-el.tar.gz ./delicious/delicious.el ./delicious/delicioapi.el ./delicious/gpl.txt ./delicious/CHANGELOG
+scp delicious-el.tar.gz freeshell.org:~/html/downloads/
