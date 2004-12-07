0b2b3053b25f68a2ac434755860b1387b19757f3
johnsu01 <>
2004-12-07 03:01:04 +0000

    removed cvs log command from tar script, using ChangeLog instead.
    20041207030104-219f2-e58a867bc9a63614891648c864364722856e9687.gz

--- lisp/delicious/maketar.sh
+++ lisp/delicious/maketar.sh
@@ -1,5 +1,4 @@
 
-cvs log > CHANGELOG
 cd /home/johnsu01/documents/cvs/code/lisp
-tar cvzf delicious-el.tar.gz ./delicious/delicious.el ./delicious/delicioapi.el ./delicious/gpl.txt ./delicious/CHANGELOG
+tar cvzf delicious-el.tar.gz ./delicious/delicious.el ./delicious/delicioapi.el ./delicious/gpl.txt ./delicious/ChangeLog
 scp delicious-el.tar.gz freeshell.org:~/html/downloads/
